d19118ea576c1282db505849b3ba89b4cb7300c3
Yassine Tioual <nisay@spamalysis.minet.net>
2016-04-19 12:31:03 +0200

    Minor changes:
    *Transform X_Envelope_From to a multivalued Object instead of String
    *Further testing on X-Envelope-From field

--- analyzers.py
+++ analyzers.py
@@ -2,10 +2,6 @@
 # _*_coding:Utf_8 _*
 import re
 
-def emailAnalyzer(email):
-    analyzed = re.search('([\w\.-]*)@([\w\.-]*)', email)
-    email = analyzed.group(0)
-    localpart = analyzed.group(1)
-    domain = analyzed.group(2)
-
-    return (email,localpart, domain)
+def emailAnalyzer(header):
+    analyzed = re.search('([\w\.-]*)@([\w\.-]*)', header)
+    return (analyzed.group(),analyzed.group(1),analyzed.group(2))
--- indexer.py
+++ indexer.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3.4
 # _*_coding:Utf_8 _*
 
-from elasticsearch_dsl import DocType, Index, String, Date, Integer, Boolean, Float, Nested
+from elasticsearch_dsl import DocType, Index, String, Date, Integer, Boolean, Float, Nested, Object
 from elasticsearch_dsl.connections import connections
 from elasticsearch.exceptions import ConnectionError
 from analyzers import emailAnalyzer
 
 class Spam(DocType):
-    X_Envelope_From = Nested(
+    X_Envelope_From = Object(
         properties={
             'header':String(),
             'email':String(),
@@ -20,14 +20,7 @@ class Spam(DocType):
     X_Spam_Score = Float()
     To = String()
     Date = Date()
-    From = Nested(
-        properties={
-            'header':String(),
-            'email':String(),
-            'localpart':String(),
-            'domain':String()
-        }
-    ) 
+    From = String()
     Reply_To = String()
     X_Priority = Integer()
     #X_Mailer = String()
@@ -42,38 +35,34 @@ class Spam(DocType):
         doc_type = 'spam'
 
     def save(self, ** kwargs):
-        return super(Spam,self).save(** kwargs)
+        return super().save(** kwargs)
 
 
 def indexMail(jsonMail, indexName, nodeIP, nodePort):
-    
+
     # Try connecting to the Elasticsearch node
     try:
         connections.create_connection(hosts=[nodeIP+':'+str(nodePort)],timeout=3)
-   
+
         # Create the mapping if the index doesn't exist
         if not Index(indexName).exists():
             Spam.init(index=indexName)
-    
+
         # Create a new mail and initialize it
-        if (jsonMail['X-Envelope-From'] != "EMPTY"):
-            newMail = Spam(X_Envelope_From.header=jsonMail['X-Envelope-From'])
+        if (jsonMail['X-Envelope-To'] != "EMPTY"):
+            newMail = Spam(X_Envelope_To=jsonMail['X-Envelope-To'])
+        if (jsonMail['X-Envelope-From'] != "EMPTY" and jsonMail['X-Envelope-From'] != "<>"):
+            newMail.X_Envelope_From.header=jsonMail['X-Envelope-From']
             analyzingResult = emailAnalyzer(jsonMail['X-Envelope-From'])
             newMail.X_Envelope_From.email = analyzingResult[0]
             newMail.X_Envelope_From.localpart = analyzingResult[1]
             newMail.X_Envelope_From.domain = analyzingResult[2]
-        if (jsonMail['X-Envelope-To'] != "EMPTY"):
-            newMail.X_Envelope_To = jsonMail['X-Envelope-To']
         if (jsonMail['X-Spam-Flag'] != "EMPTY"):
             newMail.X_Spam_Flag = jsonMail['X-Spam-Flag']
         if (jsonMail['To'] != "EMPTY"):
             newMail.To = jsonMail['To']
         if (jsonMail['From'] != "EMPTY"):
-            newMail.From.header = jsonMail['From']
-            analyzingResult = emailAnalyzer(jsonMail['From'])
-            newMail.From.email = analyzingResult[0]
-            newMail.From.localpart = analyzingResult[1]
-            newMail.From.domain = analyzingResult[2]
+            newMail.From = jsonMail['From']
         if (jsonMail['Reply-To'] != "EMPTY"):
             newMail.Reply_To = jsonMail['Reply-To']
         if (jsonMail['Content-Transfer-Encoding'] != "EMPTY"):
@@ -88,7 +77,7 @@ def indexMail(jsonMail, indexName, nodeIP, nodePort):
         newMail.MIME_Version = jsonMail['MIME-Version']
         #newMail.X_Mailer = jsonMail['X-Mailer']
         #newMail.Message = jsonMail['Message']
-    
+
         # Overwrite the index name for the new mail
         newMail.meta.index = indexName
         # Save index
