632b7d28e69c3ecc4c1c2def71db4093285211a6
Thibaut SAUTEREAU <thibaut.sautereau@telecom-sudparis.eu>
2016-04-19 12:33:19 +0200

    Add function to get phone numbers from mail body
    * returns a list of phone numbers with no duplicates

--- processer.py
+++ processer.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python3.4
 # -*-coding:Utf-8 -*
 
-def processBody(mailBody, mailHeaders, html=True):
+import re
+
+def processBody(mailBody, mailHeaders):
     """Search for some interesting content"""
     resultBody = dict()
-    resultBody['body'] = mailBody
+    resultBody['phoneNumbers'] = processPhoneNumbers(mailBody)
+    resultBody['URLs'] = processURLs(mailBody)
     return resultBody
+
+def processPhoneNumbers(mailBody):
+    """Get phone numbers from e-mail body"""
+    phoneNumbers = list()
+    r = re.compile(r'(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})')
+    for part, html in mailBody:
+        for x in r.findall(str(part)):
+            phoneNumbers.append(x)
+    return list(set(phoneNumbers))
+
+def processURLs(mailBody):
+    """Get URLs from e-mail body"""
+    URLs = list()
+    return URLs
 
